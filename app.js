bcf197a59b62fe61c82b9cfd40d507d83258f148
Diego Rodríguez <diegorbaquero@gmail.com>
2015-11-28 17:30:27 -0500

    Improved debugging, added torrent removal, fixed column max width issue, improved files displaying

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-var client = new WebTorrent({rtcConfig: {"iceServers":[{"url":"stun:23.21.150.121","urls":"stun:23.21.150.121"},{"url":"turn:global.turn.twilio.com:3478?transport=udp","username":"00bb844e6c2a07d4ed3e22a6edd6da6a715714a7c3eb118dc20246e5e0cc50c1","credential":"Wv1IxOBVhm4CGqoWYQWQ0X4Ia0Va7p2SOENv/S7M9Vg=","urls":"turn:global.turn.twilio.com:3478?transport=udp"}]}});
-var DEBUG = true;
+var client = new WebTorrent({
+    rtcConfig: {
+        "iceServers": [{
+            "url": "stun:23.21.150.121",
+            "urls": "stun:23.21.150.121"
+        }, {
+            "url": "turn:global.turn.twilio.com:3478?transport=udp",
+            "username": "00bb844e6c2a07d4ed3e22a6edd6da6a715714a7c3eb118dc20246e5e0cc50c1",
+            "credential": "Wv1IxOBVhm4CGqoWYQWQ0X4Ia0Va7p2SOENv/S7M9Vg=",
+            "urls": "turn:global.turn.twilio.com:3478?transport=udp"
+        }]
+    }
+});
+var debug = true;
 
 var prettyBytes = function (num) {
     var exponent;
@@ -16,7 +28,7 @@ var prettyBytes = function (num) {
     }
 
     exponent = Math.min(Math.floor(Math.log(num) / Math.log(1000)), units.length - 1);
-    num = (num / Math.pow(1000, exponent)).toFixed(2) * 1;
+    num = (num / Math.pow(1000, exponent)).toFixed(1) * 1;
     unit = units[exponent];
 
     return (neg ? '-' : '') + num + ' ' + unit;
@@ -29,58 +41,68 @@ angular.module('bTorrent', [], function ($compileProvider, $locationProvider) {
         requireBase: false
     }).hashPrefix('#');
 }).controller('bTorrentCtrl', function ($scope, $http, $log, $location) {
-    if(DEBUG) $log.debug($location.hash());
     $scope.client = client;
     $scope.seedIt = true;
 
-    $scope.updateAll = function() {
+    var dbg = function (string, torrent) {
+        if (debug) {
+            if (torrent)
+                $log.debug("%c" + torrent.name + " (" + torrent.infoHash + "): %c" + string, 'color: #33C3F0', 'color: #333');
+            else
+                $log.debug("%cClient: %c" + string, 'color: #33C3F0', 'color: #333');
+        }
+    };
+
+    var updateAll = function () {
         $scope.$apply();
     };
 
-    setInterval($scope.updateAll, 500);
+    setInterval(updateAll, 500);
 
-    $scope.client.done = function() {
+    $scope.client.done = function () {
         var done = true;
-        $scope.client.torrents.forEach(function(torrent) {
-            if(!torrent.done) {
+        $scope.client.torrents.forEach(function (torrent) {
+            if (!torrent.done) {
                 done = false;
             }
         });
         return done;
     };
 
-    $scope.client.downloading = function() {
+    $scope.client.downloading = function () {
         var downloading = true;
-        $scope.client.torrents.forEach(function(torrent) {
-            if(torrent.done) {
+        $scope.client.torrents.forEach(function (torrent) {
+            if (torrent.done) {
                 downloading = false;
             }
         });
         return downloading;
     };
 
-    $scope.uploadFile = function() {
+    $scope.uploadFile = function () {
         document.getElementById("fileUpload").click();
     };
-    $scope.uploadFile2 = function(elem) {
+    $scope.uploadFile2 = function (elem) {
         $scope.client.processing = true;
-        $scope.client.seed(elem.files, $scope.onTorrent);
+        dbg("Seeding " + elem.files[0].name);
+        $scope.client.seed(elem.files, $scope.onSeed);
     };
 
-    $scope.fromInput = function() {
-        if(!$scope.torrentInput == "") {
+    $scope.fromInput = function () {
+        if (!$scope.torrentInput == "") {
             $scope.client.processing = true;
+            dbg("Adding " + $scope.torrentInput);
             $scope.client.add($scope.torrentInput, $scope.onTorrent);
             $scope.torrentInput = "";
         }
     };
 
-    $scope.toggleTorrent = function(torrent) {
-        if(torrent.showFiles) {
+    $scope.toggleTorrent = function (torrent) {
+        if (torrent.showFiles) {
             torrent.showFiles = false;
             $scope.sTorrent = null;
         } else {
-            $scope.client.torrents.forEach(function(t) {
+            $scope.client.torrents.forEach(function (t) {
                 t.showFiles = false;
             });
             torrent.showFiles = true;
@@ -88,14 +110,19 @@ angular.module('bTorrent', [], function ($compileProvider, $locationProvider) {
         }
     };
 
-    $scope.onTorrent = function(torrent) {
+    $scope.destroyedTorrent = function (err) {
+        if (err) throw err;
+        dbg("Destroyed torrent");
+    };
+
+    $scope.onTorrent = function (torrent, isSeed) {
         $scope.client.processing = false;
         torrent.showFiles = false;
         torrent.pSize = prettyBytes(torrent.length);
         torrent.fileName = torrent.name + '.torrent';
         torrent.oTorrentFileURL = torrent.torrentFileURL;
 
-        if(angular.isUndefined($scope.sTorrent) || $scope.sTorrent === null) {
+        if (angular.isUndefined($scope.sTorrent) || $scope.sTorrent === null) {
             $scope.sTorrent = torrent;
             torrent.showFiles = true;
         }
@@ -114,8 +141,6 @@ angular.module('bTorrent', [], function ($compileProvider, $locationProvider) {
             }
         };
 
-        if(DEBUG) $log.debug("Downloading..." + torrent.infoHash);
-
         torrent.files.forEach(function (file) {
             file.pSize = prettyBytes(file.length);
             file.status = "Downloading";
@@ -123,42 +148,41 @@ angular.module('bTorrent', [], function ($compileProvider, $locationProvider) {
             file.getBlobURL(function (err, url) {
                 if (err) throw err;
                 file.url = url;
-                if(DEBUG) $log.debug("Got BLOB " + file.url);
+                if(!isSeed) dbg("Finished downloading file " + file.name, torrent);
                 file.status = "Ready";
                 $scope.$apply();
             });
-            if(DEBUG) $log.debug("FILE");
+            if(!isSeed) dbg("Received file " + file.name + " metadata", torrent);
         });
 
         torrent.on('download', function (chunkSize) {
-            if(DEBUG) $log.debug("DOWNLOAD");
-            torrent.update();
+            if(!isSeed) dbg("Downloaded chunk", torrent);
         });
 
         torrent.on('upload', function (chunkSize) {
-            if(DEBUG) $log.debug("UPLOAD");
-            torrent.update();
+            dbg("Uploaded chunk", torrent);
         });
 
         torrent.on('done', function () {
-            if(DEBUG) $log.debug("DONE");
+            if(!isSeed) dbg("Done", torrent);
             torrent.update();
         });
 
         torrent.on('wire', function (wire, addr) {
-            if(DEBUG) $log.debug("WIRE");
-        });
-
-        torrent.on('wire', function (wire, addr) {
-            console.log('connected to peer with address ' + addr);
+            dbg("Wire " + addr, torrent);
         });
 
         setInterval(torrent.update, 500);
         torrent.update();
     };
 
-    if($location.hash() != '') {
+    $scope.onSeed = function(torrent) {
+        $scope.onTorrent(torrent, true);
+    };
+
+    if ($location.hash() != '') {
         $scope.client.processing = true;
+        dbg("Adding " + $location.hash());
         client.add($location.hash(), $scope.onTorrent);
     }
 }).filter('html', function ($sce) {
