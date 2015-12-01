00609e4bd0791322b5d6c169d263e70d50f73eb7
David Baldwynn <david.baldwin@alumni.ubc.ca>
2015-12-01 15:04:11 -0800

    fixed style and codeclimeate bug errors

--- karma.conf.js
+++ karma.conf.js
@@ -3,77 +3,76 @@
 /**
  * Module dependencies.
  */
-var _ = require('lodash'),
-  karmaReporters = ['progress', 'mocha', 'html', 'coverage'];
+var karmaReporters = ['progress', 'mocha', 'html', 'coverage'];
 
 // Karma configuration
 module.exports = function (karmaConfig) {
-  karmaConfig.set({
-    // Frameworks to use
-    frameworks: ['jasmine'],
+	karmaConfig.set({
+		// Frameworks to use
+		frameworks: ['jasmine'],
 
-    preprocessors: {
-      'test/**/*.js': ['coverage'],
-		  'www/**.html': ['ng-html2js'],
+		preprocessors: {
+			'test/**/*.js': ['coverage'],
+			'www/**.html': ['ng-html2js']
 		},
 
-		ngHtml2JsPreprocessor: {
-		    stripPrefix: 'www/',
+	ngHtml2JsPreprocessor: {
+			stripPrefix: 'www/',
 
-		    // the name of the Angular module to create
-    		moduleName: 'templates'
+			// the name of the Angular module to create
+			moduleName: 'templates'
 		},
 
-    // List of files / patterns to load in the browser
-    files: ['www/**.html', 'www/**.js', 'www/**.css'],
+		// List of files / patterns to load in the browser
+		files: ['www/**.html', 'www/**.js', 'www/**.css'],
 
-    // Test results reporter to use
-    // Possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: karmaReporters,
+		// Test results reporter to use
+		// Possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+		reporters: karmaReporters,
 
-    // Configure the coverage reporter
-    coverageReporter: {
-      dir : 'coverage/client',
-      reporters: [
-        // Reporters not supporting the `file` property
-        { type: 'html', subdir: 'report-html' },
-        { type: 'lcov', subdir: 'report-lcov' },
-        // Output coverage to console
-        { type: 'text' }
-      ],
-      instrumenterOptions: {
-        istanbul: { noCompact: true }
-      }
-    },
+		// Configure the coverage reporter
+		coverageReporter: {
+			dir : 'coverage/client',
+			reporters: [
+				// Reporters not supporting the `file` property
+				{ type: 'html', subdir: 'report-html' },
+				{ type: 'lcov', subdir: 'report-lcov' },
+				// Output coverage to console
+				{ type: 'text' }
+			],
+			instrumenterOptions: {
+				istanbul: { noCompact: true }
+			}
+		},
 
-    // Web server port
-    port: 9876,
+		// Web server port
+		port: 9876,
 
-    // Enable / disable colors in the output (reporters and logs)
-    colors: true,
+		// Enable / disable colors in the output (reporters and logs)
+		colors: true,
 
-    // Level of logging
-    // Possible values: karmaConfig.LOG_DISABLE || karmaConfig.LOG_ERROR || karmaConfig.LOG_WARN || karmaConfig.LOG_INFO || karmaConfig.LOG_DEBUG
-    logLevel: karmaConfig.LOG_INFO,
+		// Level of logging
+		// Possible values: karmaConfig.LOG_DISABLE || karmaConfig.LOG_ERROR || karmaConfig.LOG_WARN || karmaConfig.LOG_INFO || karmaConfig.LOG_DEBUG
+		logLevel: karmaConfig.LOG_INFO,
 
-    // Enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+		// Enable / disable watching file and executing tests whenever any file changes
+		autoWatch: true,
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
-    browsers: ['PhantomJS'],
+		// Start these browsers, currently available:
+		// - Chrome
+		// - ChromeCanary
+		// - Firefox
+		// - Opera
+		// - Safari (only Mac)
+		// - PhantomJS
+		// - IE (only Windows)
+		browsers: ['PhantomJS'],
 
-    // If browser does not capture in given timeout [ms], kill it
-    captureTimeout: 60000,
+		// If browser does not capture in given timeout [ms], kill it
+		captureTimeout: 60000,
 
-    // Continuous Integration mode
-    // If true, it capture browsers, run tests and exit
-    singleRun: true
-  });
+		// Continuous Integration mode
+		// If true, it capture browsers, run tests and exit
+		singleRun: true
+	});
 }
