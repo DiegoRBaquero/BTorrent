9f9dc4eec3e8c50d5bdd9a6869f7c857079634e9
Code Hangen <111701513+codehangen@users.noreply.github.com>
2022-12-14 23:53:58 -0300

    Update modeTheme.js

--- modeTheme.js
+++ modeTheme.js
@@ -1,14 +1,5 @@
 // license: https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp
-function darkTheme() { 
+function darkModetheme() { 
    var element = document.body;
    element.classList.toggle("dark-mode");
-   var button = document.querySelector("button")
-   button.classList.add("dark-modeBtn")
-}
-
-function lightTheme() { 
-   var body = document.querySelector("body")
-   body.classList.remove("dark-mode")
-   var button = document.querySelector("button")
-   button.classList.remove("dark-modeBtn")
 }
