7688e1b7b9a6be4341d1b9f409fa80e8128c99a1
Code Hangen <111701513+codehangen@users.noreply.github.com>
2022-12-14 22:02:44 -0300

    Update modeTheme.js

--- modeTheme.js
+++ modeTheme.js
@@ -1,4 +1,10 @@
 function modeTheme() {
    var element = document.body;
    element.classList.toggle("dark-mode");
+   document.getElementById("btn1").style.color = "white";
+   document.getElementById("btn2").style.color = "white";
+   document.getElementById("btn3").style.color = "white";
+   document.getElementById("btn4").style.color = "white";
+   document.getElementById("btn5").style.color = "white";
+   document.getElementById("btn6").style.color = "white";
 }
